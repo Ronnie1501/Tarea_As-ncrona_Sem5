ea53aa75966750e55d198c1d005f06cff7b3972e
Ronnie1501 <124007445+Ronnie1501@users.noreply.github.com>
2023-02-25 08:04:15 -0600

    Tarea_Asíncrona_5

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Console.Write("ingresar el valor con que desea iniciar: ");
+           int valorinicial = int.Parse(Console.ReadLine());
+
+           Console.Write("ingrese el valor final: ");
+           int valorFinal = int.Parse(Console.ReadLine());
+
+           Console.Write("los numeros pares ingresados son: ");
+
+           for (int i = valorinicial; i <= valorFinal; i++)
+           {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+           }    
+
+    
+
+
+
+
+
+
